d1a07f13b94235ca4dd48d324349d7072ca86e1e
Alejandro Garcia Serna <alejandrogarcia@seven4n.com>
2021-03-05 18:29:15 -0500

    Improve file look up

--- filemanagement/filemanagement.go
+++ filemanagement/filemanagement.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 // FilesManagement struct to manipulate folder files information
@@ -25,10 +26,11 @@ func NewFilesManagement(folderPath string) (FilesManagement, error) {
 
 // FileInformation simple struct to keep general information of a file
 type FileInformation struct {
-	ID   string `json:"id"`
-	Path string `json:"path"`
-	Name string `json:"name"`
-	Size int64  `json:"size"`
+	ID        string `json:"id"`
+	Path      string `json:"path"`
+	ShortPath string `json:"short_path"`
+	Name      string `json:"name"`
+	Size      int64  `json:"size"`
 }
 
 // FileBinaryMetadata hold OS file metadata
@@ -87,13 +89,38 @@ func (fm *FilesManagement) traverseDirectory(files *[]FileInformation) filepath.
 
 		if !info.IsDir() {
 			*files = append(*files, FileInformation{
-				ID:   info.Name() + "-" + strconv.FormatInt(info.Size(), 10),
-				Name: info.Name(),
-				Path: path,
-				Size: info.Size(),
+				ID:        info.Name() + "-" + strconv.FormatInt(info.Size(), 10),
+				Name:      info.Name(),
+				Path:      path,
+				Size:      info.Size(),
+				ShortPath: fm.buildFileShortPath(path),
 			})
 		}
 
+		// Avoid hidden files, those which begins with a dot(.) are included in the final response
+		var notHiddenFiles []FileInformation = []FileInformation{}
+		for _, gotFile := range *files {
+			fileTree := strings.Split(gotFile.Path, "/")
+			hasPoint := false
+
+			for _, treePart := range fileTree {
+				if strings.HasPrefix(treePart, ".") {
+					hasPoint = true
+					break
+				}
+			}
+
+			if !hasPoint {
+				notHiddenFiles = append(notHiddenFiles, gotFile)
+			}
+		}
+
+		*files = notHiddenFiles
+
 		return nil
 	}
+}
+
+func (fm *FilesManagement) buildFileShortPath(filePath string) string {
+	return strings.Replace(filePath, fm.folder, "", 1)[1:]
 }
